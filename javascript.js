f50f80e20c765ba9db4f16d35697f4a56b386e5d
Codrius <magefire342@gmail.com>
2023-08-07 15:12:03 -0400

    Got gamehealth working!

--- javascript.js
+++ javascript.js
@@ -1,11 +1,18 @@
 const paragraph = document.querySelector(".dynamicData");
-let openPrice;
-let closePrice;
 
 axios.get("https://raw.githubusercontent.com/Codrius/AdVUEntures/main/stockdata.json")
     .then(stockjson => {
         const stockdata = stockjson.data;
-        openPrice = stockdata["Time Series (Daily)"]["2013-08-04"]["1. open"];
-        paragraph.textContent = openPrice;
+        const openPrice = stockdata["Time Series (Daily)"]["2023-08-04"]["1. open"];
+        const closePrice = stockdata["Time Series (Daily)"]["2023-08-04"]["4. close"];
+        const stockChange = parseFloat(openPrice) - parseFloat(closePrice);
+        
+        if (stockChange>0) {
+            paragraph.textContent = `If you look at the stock values, the stock increased by $${stockChange} yesterday.  This is indicative of how the game is absolutely BOOMING right now, and is in the best state it has ever been.`;
+        } else if (stockChange<0) {
+            paragraph.textContent = `If you look at the stock values, the stock decreased by $${stockChange} yesterday.  This is indicative of how the game is absolutely DYING right now, and is in the worse state it has ever been.`
+        } else {
+            paragraph.textContent = `If you look at the stock values, the stock didn't change AT ALL yesterday. This is indicative of how the game is absolutely STAGNANT right now, and is in the most boring state it has ever been.`
+        }
     })
     .catch(error => {console.error(error)})
