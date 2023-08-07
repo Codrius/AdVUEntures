33b23a99cb60b8cd70c60ceadbfa83095a306bdc
Codrius <magefire342@gmail.com>
2023-08-07 14:46:41 -0400

    Committing more bugs for more help

--- javascript.js
+++ javascript.js
@@ -1,12 +1,11 @@
-import axios from 'axios';
-
 const paragraph = document.querySelector(".dynamicData");
 let openPrice;
 let closePrice;
 
 axios.get("https://raw.githubusercontent.com/Codrius/AdVUEntures/main/stockdata.json")
     .then(stockjson => {
-        const data = stockjson.data;
-        paragraph.textContent = data
+        const stockdata = JSON.parse(stockjson);
+        openPrice = stockdata["Time Series (Daily)"]["2013-08-04"]["1. open"];
+        paragraph.textContent = openPrice;
     })
     .catch(error => {console.error(error)})
